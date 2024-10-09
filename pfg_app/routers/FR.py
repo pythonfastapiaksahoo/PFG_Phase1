1269fcc58d9753917f8006523c019e83848b6106
Jijo Joseph <jijo.joseph@datasemantics.co>
2024-10-09 06:17:50 +0400

    moved unnecessary from fields.json changed ocr model to prebuilt-layout

--- pfg_app/routers/FR.py
+++ pfg_app/routers/FR.py
@@ -163,7 +163,6 @@ async def update_metadata(
                 "fieldKey": "tab_1_object",
                 "fieldType": "object",
                 "fieldFormat": "not-specified",
-                "itemType": None,
                 "fields": [],
             }
         }
@@ -175,8 +174,6 @@ async def update_metadata(
                         "fieldKey": m,
                         "fieldType": "string",
                         "fieldFormat": "not-specified",
-                        "itemType": None,
-                        "fields": None,
                     }
                 )
                 lineitemupdates.append(m)
@@ -187,8 +184,6 @@ async def update_metadata(
                         "fieldKey": m,
                         "fieldType": "string",
                         "fieldFormat": "not-specified",
-                        "itemType": None,
-                        "fields": None,
                     }
                 )
                 lineitemupdates.append(m)
@@ -249,7 +244,6 @@ async def update_metadata(
                     "fieldType": "array",
                     "fieldFormat": "not-specified",
                     "itemType": "tab_1_object",
-                    "fields": None,
                 }
             )
             jso = {
