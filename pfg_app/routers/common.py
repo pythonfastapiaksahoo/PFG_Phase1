168644b486a7737fccdfd321140138c3d99ea098
Jijo Joseph <jijo.joseph@datasemantics.co>
2024-10-14 22:29:27 +0400

    fetching secrets from key vault

--- pfg_app/routers/common.py
+++ pfg_app/routers/common.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Depends, Response
 
+# from pfg_app import settings
 from pfg_app.azuread.auth import get_admin_user
+
+# from pfg_app.core import azure_fr as core_fr
 from pfg_app.core.utils import get_blob_securely
 
 router = APIRouter(
@@ -34,3 +37,22 @@ def get_blob_file(container_name: str, blob_path: str):
         "Content-Type": content_type,
     }
     return Response(content=blob_data, headers=headers, media_type=content_type)
+
+
+# @router.get("/call-azure-document-intelligence")
+# def call_azure_document_intelligence(container_name: str, blob_path: str):
+#     """API route to call the Azure Document Intelligence API.
+
+#     Returns:
+#     -------
+#     Response from the Azure Document Intelligence API.
+#     """
+#     file_data, content_type = get_blob_securely(
+#         container_name=container_name, blob_path=blob_path
+#     )
+#     response = core_fr.call_form_recognizer(
+#         file_data=file_data,
+#         endpoint=settings.form_recognizer_endpoint,
+#         api_version=settings.api_version,
+#     )
+#     return response
