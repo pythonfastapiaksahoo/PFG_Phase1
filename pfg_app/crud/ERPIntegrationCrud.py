faad1eac819c15ffd3ad9e6b556fd7616a86add1
mayankb <mayank.bhatia@datasemantics.co>
2024-11-15 21:26:05 +0530

    voucher payload is fixed

--- pfg_app/crud/ERPIntegrationCrud.py
+++ pfg_app/crud/ERPIntegrationCrud.py
@@ -885,8 +885,16 @@ def processInvoiceVoucher(doc_id, db):
                                         else 0
                                     ),
                                     "SALETX_AMT": 0,
-                                    "FREIGHT_AMT": voucherdata.freight_amt or "",
-                                    "MISC_AMT": voucherdata.misc_amt or "",
+                                    "FREIGHT_AMT": (
+                                        voucherdata.freight_amt
+                                        if voucherdata.freight_amt
+                                        else 0
+                                    ),
+                                    "MISC_AMT": (
+                                        voucherdata.misc_amt
+                                        if voucherdata.misc_amt
+                                        else 0
+                                    ),
                                     "PYMNT_TERMS_CD": "",
                                     "TXN_CURRENCY_CD": voucherdata.currency_code or "",
                                     "VAT_ENTRD_AMT": (
