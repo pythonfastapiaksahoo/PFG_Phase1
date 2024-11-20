77cf2188fbc5730739b699a3d09f6dab98b16abe
mayankb <mayank.bhatia@datasemantics.co>
2024-11-20 20:24:19 +0530

    version updated to 0..38

--- pfg_app/main.py
+++ pfg_app/main.py
@@ -175,7 +175,7 @@ async def add_operation_id(request: Request, call_next):
             response = await call_next(request)
             response.headers["x-operation-id"] = operation_id or "unknown"
 
-            response.headers["api-version"] = "0.38"
+            response.headers["api-version"] = "0.39"
 
             logger.info(
                 "Sending response from FastAPI"
