b371e163c950aed9d9a4594edf0f8cc81f70c953
mayankb <mayank.bhatia@datasemantics.co>
2024-11-30 02:05:28 +0530

    version updated to 0.61

--- pfg_app/main.py
+++ pfg_app/main.py
@@ -177,7 +177,7 @@ async def add_operation_id(request: Request, call_next):
             response.headers["x-operation-id"] = operation_id or "unknown"
 
 
-            response.headers["api-version"] = "0.60"
+            response.headers["api-version"] = "0.61"
 
 
             logger.info(
