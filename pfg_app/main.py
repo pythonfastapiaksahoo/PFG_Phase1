0384078dfed62fb2c43771639a2269ebd021e3c5
mayankb <mayank.bhatia@datasemantics.co>
2025-02-19 20:52:36 +0530

    paginated corp document list api is developed

--- pfg_app/main.py
+++ pfg_app/main.py
@@ -221,7 +221,7 @@ async def add_operation_id(request: Request, call_next):
             response = await call_next(request)
             response.headers["x-operation-id"] = operation_id or "unknown"
 
-            response.headers["api-version"] = "0.98.4"
+            response.headers["api-version"] = "0.98.5"
 
             logger.info(
                 "Sending response from FastAPI"
