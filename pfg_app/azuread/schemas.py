2ab9f2435b16dba7074017c5a83bbad75837e78f
mayankb <mayank.bhatia@datasemantics.co>
2025-03-07 00:13:38 +0530

    tmid extraction added and doc_updates added in corpdoc data

--- pfg_app/azuread/schemas.py
+++ pfg_app/azuread/schemas.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 class AzureUser(BaseModel):
     id: str
     name: str
-    email: str
+    email: Optional[str] = None
     preferred_username: str
     roles: List[str]
     employeeId: Optional[str] = None
