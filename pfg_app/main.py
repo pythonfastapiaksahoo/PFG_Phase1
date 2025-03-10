63fac14847488cb35694ef82f4ccdc4db8678d27
mayankb <mayank.bhatia@datasemantics.co>
2025-03-10 23:03:20 +0530

    corp queue task fixes

--- pfg_app/main.py
+++ pfg_app/main.py
@@ -230,7 +230,7 @@ async def add_operation_id(request: Request, call_next):
             response.headers["x-operation-id"] = operation_id or "unknown"
 
 
-            response.headers["api-version"] = "0.100.23"
+            response.headers["api-version"] = "0.100.24"
 
 
             logger.info(
