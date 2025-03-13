02a24321981099c8933bc3d5998928429145bec8
yesh <yeshaswini@datasemantics.co>
2025-03-13 19:01:01 +0530

    handling multiple approvals

--- pfg_app/main.py
+++ pfg_app/main.py
@@ -230,7 +230,7 @@ async def add_operation_id(request: Request, call_next):
             response.headers["x-operation-id"] = operation_id or "unknown"
 
 
-            response.headers["api-version"] = "0.100.59"
+            response.headers["api-version"] = "0.100.60"
 
 
             logger.info(
