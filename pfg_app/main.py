28d697969d310d4222d609e71a317250c9056128
yesh <yeshaswini@datasemantics.co>
2025-03-14 15:33:20 +0530

    vendor id update

--- pfg_app/main.py
+++ pfg_app/main.py
@@ -230,7 +230,7 @@ async def add_operation_id(request: Request, call_next):
             response.headers["x-operation-id"] = operation_id or "unknown"
 
 
-            response.headers["api-version"] = "0.100.65"
+            response.headers["api-version"] = "0.100.66"
 
 
             logger.info(
