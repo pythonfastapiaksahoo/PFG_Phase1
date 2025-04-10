7886af58836c99dac0a177957ccd238ae09e34ee
mayankb <mayank.bhatia@datasemantics.co>
2025-04-10 19:24:50 +0530

    prompt is tuned for extracting GST correctly

--- pfg_app/core/openai_data.py
+++ pfg_app/core/openai_data.py
@@ -94,8 +94,6 @@ def extract_invoice_details_using_openai(blob_data):
                 - **InvoiceDate**: Extract the invoice date only from the invoice document and exclude time if present. for example, '01/27/2025 15:53:19' then extract '01/27/2025'.
                 - **Currency**: Identified by currency symbols (e.g., CAD, USD). If the currency is not explicitly identified as USD, default to CAD.
                 - **GST**: Extracted 'GST' or 'Goods and Services Tax' or 'Tax' from invoice document if present else return "N/A",
-                            if TAX is present in the invoice document, extract the value after TAX.
-                            For example- 'Tax $2.23' or 'GST 2.23' or 'Goods and Services Tax' 2.23 then extract 2.23.
                 - **PST**: Extracted PST from invoice document if present else return "N/A".
                 - **PST-SK**: Extracted PST-SK from invoice document if present else return "N/A".
                 - **PST-BC**: Extracted PST-BC from invoice document if present else return "N/A".
@@ -120,6 +118,8 @@ def extract_invoice_details_using_openai(blob_data):
                         - if the vendor address is not present in the invoice document, return "N/A".
                     - **CreditNote** : if "Credit Memo" or Credit Note" is present in the invoice document, then return "Yes".
                         - if the invoice total fields is in negative or in braces, then return "Yes".for example, '-123.45' or '123.45-' or (123.45) return "Yes".
+                    - **GST** : Extracted 'GST' or 'Goods and Services Tax' or 'Tax' from invoice document if present else return "N/A",
+                        - if "TAX" is present in the invoice document, extract the value after TAX. for example: 'Tax $2.23' then extract 2.23.
                     - Ensure that the amounts(Subtotal,invoicetotal,GST,PST and other charges) to be extracted from last page only if  multiple amounts details are present in line items of all the pages. 
                 4. **Output Format**: Ensure that the JSON output is precise and clean, without any extra text or commentary like ```json```,  it will be processed using json.loads.
 
