2404b32706ec3f72703635153f84d02a8898cd9c
mayankb <mayank.bhatia@datasemantics.co>
2025-04-26 00:04:48 +0530

    function to mark mail as read moved to process message function.

--- pfg_app/graph_api/mark_processed_mail_to_read.py
+++ pfg_app/graph_api/mark_processed_mail_to_read.py
@@ -14,20 +14,9 @@ from pfg_app import settings
 
 
 
-def mark_processed_mail_as_read(mail_row_key):
+def mark_processed_mail_as_read(new_mail_id, message_id):
     try:
-        logger.info(f"Marking mail as read for mail_row_key: {mail_row_key} initiated.")
-        db = next(get_db())
-        mail_id_str = mail_row_key.split("-")[-1]
-        mail_id = int(mail_id_str)
-
-        # Query CorpMail to get message_id
-        corp_mail = db.query(model.CorpMail).filter(model.CorpMail.id == mail_id).first()
-        if not corp_mail:
-            logger.error(f"CorpMail with id {mail_id} not found")
-
-        message_id = corp_mail.message_id
-        logger.info(f"Marking mail as read")
+        logger.info(f"Marking mail as read for mail_row_key: {new_mail_id} initiated.")
         # Get access token
         token_manager = MSGraphAPITokenManager()
         access_token = token_manager.get_access_token()
@@ -40,8 +29,8 @@ def mark_processed_mail_as_read(mail_row_key):
             logger.info(f"Mail folder IDP found: {mail_folder_id}")
         else:
             logger.info(f"Mail folder IDP not found, creating a new one Manually")
-            return False
-
+            return "failed"
+        
         # Call Graph API to mark mail as read
         patch_url = (
             f"https://graph.microsoft.com/v1.0/users/{settings.graph_corporate_mail_id}/mailFolders/{mail_folder_id}/messages/{message_id}"
@@ -55,11 +44,60 @@ def mark_processed_mail_as_read(mail_row_key):
             # raise HTTPException(status_code=response.status_code, detail="Failed to update message status")
             logger.error(f"Failed to update message status: {response.status_code}")
             return "failed"
-            # return {"error": "Failed to update message status"}
-            
+        
         return "success"
 
     except Exception as e:
         logger.error(f"Error marking mail as read: {traceback.format_exc()}")
         return "failed"
         
+
+
+# def mark_processed_mail_as_read(mail_row_key):
+#     try:
+#         logger.info(f"Marking mail as read for mail_row_key: {mail_row_key} initiated.")
+#         db = next(get_db())
+#         mail_id_str = mail_row_key.split("-")[-1]
+#         mail_id = int(mail_id_str)
+
+#         # Query CorpMail to get message_id
+#         corp_mail = db.query(model.CorpMail).filter(model.CorpMail.id == mail_id).first()
+#         if not corp_mail:
+#             logger.error(f"CorpMail with id {mail_id} not found")
+
+#         message_id = corp_mail.message_id
+#         logger.info(f"Marking mail as read")
+#         # Get access token
+#         token_manager = MSGraphAPITokenManager()
+#         access_token = token_manager.get_access_token()
+#         headers = {
+#             "Authorization": f"Bearer {access_token}",
+#             "Content-Type": "application/json"
+#         }
+#         mail_folder_id = get_folder_id("IDP", access_token)
+#         if mail_folder_id:
+#             logger.info(f"Mail folder IDP found: {mail_folder_id}")
+#         else:
+#             logger.info(f"Mail folder IDP not found, creating a new one Manually")
+#             return "failed"
+
+#         # Call Graph API to mark mail as read
+#         patch_url = (
+#             f"https://graph.microsoft.com/v1.0/users/{settings.graph_corporate_mail_id}/mailFolders/{mail_folder_id}/messages/{message_id}"
+#         )
+#         body = {
+#             "isRead": True
+#         }
+
+#         response = requests.patch(patch_url, json=body, headers=headers)
+#         if response.status_code != 200:
+#             # raise HTTPException(status_code=response.status_code, detail="Failed to update message status")
+#             logger.error(f"Failed to update message status: {response.status_code}")
+#             return "failed"
+#             # return {"error": "Failed to update message status"}
+            
+#         return "success"
+
+#     except Exception as e:
+#         logger.error(f"Error marking mail as read: {traceback.format_exc()}")
+#         return "failed"
